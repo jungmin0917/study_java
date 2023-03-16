6a5109292db378af080c7d03f601f283b88fa8c3
jungmin0917 <cloonds@gmail.com>
2023-03-16 17:26:36 +0900

    until day14

--- workspace/baekjoon/src/baekjoon/Main.java
+++ workspace/baekjoon/src/baekjoon/Main.java
@@ -6,15 +6,21 @@ import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.StreamTokenizer;
+import java.util.Arrays;
 
 public class Main {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); // 입력 관련
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); // 출력 관련
+
+		String[] array = br.readLine().split(" ");
+		long[] nums = new long[array.length];
 		
-		String str = br.readLine();
+		for (int i = 0; i < nums.length; i++) {
+			nums[i] = Long.parseLong(array[i]);
+		}
 		
-		bw.write(str.length() + ""); // 문자열을 출력하는 메소드이므로 문자열로 변환해줘야 함
+		bw.write(Arrays.stream(nums).sum()+"");
 		
 		bw.flush(); // 버퍼를 비워 출력을 실제로 한다
 		bw.close(); // 버퍼를 닫는다
