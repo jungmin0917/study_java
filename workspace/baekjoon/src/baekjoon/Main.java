db4e6cadffcc51c6a88bfdcb0c04815582c1ca95
jungmin0917 <cloonds@gmail.com>
2023-03-17 12:45:48 +0900

    until day22

--- workspace/baekjoon/src/baekjoon/Main.java
+++ workspace/baekjoon/src/baekjoon/Main.java
@@ -1,24 +1,29 @@
 package baekjoon;
 
-import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.Scanner;
+import java.math.BigInteger;
 import java.util.StringTokenizer;
 
 public class Main {
 	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); // 입력 관련
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); // 출력 관련
-
-		Scanner sc = new Scanner(System.in);
 		
-		int n = sc.nextInt();
-		int res = n - 1946;
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
-		System.out.println(res);
+		String input = br.readLine();
+		
+		StringTokenizer st = new StringTokenizer(input);
+		
+		BigInteger a = new BigInteger(st.nextToken());
+		BigInteger b = new BigInteger(st.nextToken());
+		
+		BigInteger sum = a.add(b);
+		
+		bw.write(sum + "");
 		
 		bw.flush();
 		bw.close();
