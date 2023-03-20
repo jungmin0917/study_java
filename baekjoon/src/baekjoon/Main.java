e3ab65efa9bb11a93570fb86f15d26dd483a8ab3
jungmin0917 <cloonds@gmail.com>
2023-03-20 15:33:18 +0900

    jsp day02

--- baekjoon/src/baekjoon/Main.java
+++ baekjoon/src/baekjoon/Main.java
@@ -5,7 +5,6 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.math.BigInteger;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -14,13 +13,11 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
-		BigInteger a = new BigInteger(br.readLine());
-		BigInteger b = new BigInteger(br.readLine());
+		int n = Integer.parseInt(br.readLine());
 		
-		BigInteger total = a.multiply(BigInteger.valueOf(8)).add(b.multiply(BigInteger.valueOf(3)));
-		BigInteger res = total.subtract(BigInteger.valueOf(28));
+		String res = "SciComLove\n".repeat(n);
 		
-		bw.write(res + "");
+		bw.write(res);
 		
 		bw.flush();
 		bw.close();
