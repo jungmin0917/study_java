d35074e0fe072ffe95679d348f8d9abe4df198c9
jungmin0917 <cloonds@gmail.com>
2023-04-01 18:20:19 +0900

    JSP2 until day23 halfway

---
+++ java_jsp2/board_mvc/src/main/java/com/jungmin/app/board/dao/BoardDAO.java
@@ -0,0 +1,30 @@
+package com.jungmin.app.board.dao;
+
+import java.util.HashMap;
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+
+import com.jungmin.app.board.vo.BoardVO;
+import com.jungmin.app.mybatis.config.MyBatisConfig;
+
+public class BoardDAO {
+	SqlSessionFactory sqlSessionFactory = MyBatisConfig.getSqlSessionFactory();
+	SqlSession sqlSession;
+	
+	// 기본 생성자
+	public BoardDAO() {
+		sqlSession = sqlSessionFactory.openSession(true); // auto commit 옵션 켬
+	}
+	
+	// 게시글 목록
+	public List<BoardVO> getList(int startRow, int endRow) {
+		HashMap<String, Integer> pageMap = new HashMap<>();
+		
+		pageMap.put("startRow", startRow);
+		pageMap.put("endRow", endRow);
+		
+		return sqlSession.selectList("Board.getList", pageMap);
+	}
+}
